b32232b3b0bf84ac1f609116e5afa5e06785817c
syazantri <syazantri@gmail.com>
2024-02-13 13:21:31 +0700

    refactor: remove unnecessary modifier in ProductService.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,9 +4,9 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
-    public Product findById(String id);
-    public Product edit(Product product);
+    Product create(Product product);
+    List<Product> findAll();
+    Product findById(String id);
+    Product edit(Product product);
     void delete(String id);
 }
